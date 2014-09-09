d63df9bf07dc61ffa542ab317350acb4520abb09
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-09-08 23:50:20 -0400

    added template model

--- themes/models.py
+++ themes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Template(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.ForeignKey(User)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    template = models.TextField(blank=True)
+    styles = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
