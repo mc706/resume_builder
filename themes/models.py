2c34ef086af8ab75cc7933a84229617f9e68cb6e
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-09-09 08:31:13 -0400

    updated template model

--- themes/models.py
+++ themes/models.py
@@ -7,8 +7,9 @@ class Template(models.Model):
     creator = models.ForeignKey(User)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    template = models.TextField(blank=True)
     styles = models.TextField(blank=True)
 
     def __unicode__(self):
         return self.name
+
+    
